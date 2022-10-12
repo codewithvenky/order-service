ec5ea17fefd3e228b106792ac350d55c3558eeba
codewithvenky <codewithvenky@gmail.com>
2022-10-12 16:35:50 +0530

    removing lombak

--- src/main/java/com/javatechie/aws/cicd/example/Order.java
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -15,4 +15,56 @@ public class Order {
     private String name;
     private int quantity;
     private long price;
+
+    public Order() {
+    }
+
+    public Order(int id, String name, int quantity, long price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
 }
